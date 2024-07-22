468cc1715c30c74c460381f28d1b65295e47fde7
Mushroom-MSL1L <mushroom.ac.cs11@nycu.edu.tw>
2024-07-22 22:08:46 +0800

    modify : modify convert_data functions
    1. pick certain data from internet data to reduce row data amount
    2. add comment

--- Database/convert_data.py
+++ Database/convert_data.py
@@ -4,29 +4,66 @@ from datetime import datetime, timedelta
 import re 
 
 """
-Define all custom functions for "get_data.py", to load internet data to database.
+Purpose of this file:
+    Define all custom functions for "get_data.py", to load internet data to database.
+    Should refer to "data_list.md" for more information of each original data structure.
 """
 
+"""
+    "convert_and_store_ETagPairLive" : 
+        Process ETagPairLive data from xml to separated data and store in database
+    data file : xml
+    processed type(not return) : store in database (default name : row.db)
+    data information:
+        data name : "eTag 配對路徑動態資訊(v2.0)" 的任意一份資料當作格式範例
+        reference data link : https://tisvcloud.freeway.gov.tw/history/motc20/ETag/20240302/ETagPairLive_2330.xml.gz
+        data source page link : https://tisvcloud.freeway.gov.tw/history/motc20/ETag/
+    procedure : 
+        1. parse xml file as web data
+        2. create ETagPairLive table
+        3. extract first level data from "etag_pair_live" xml
+            * drop uneeded row_data (both start and end status should be normal)
+        4. extract second level data from "flow" xml
+            * drop uneeded vehicle type data
+            * process ETagPairID to 
+                * highway as one digit
+                * start_mileage as km
+                * end_mileage as km
+                * direction as char
+            * process time to year, month, day, five_minute
+        
+"""
 def convert_and_store_ETagPairLive(store_path, conn):
-    """
-    Process ETagPairLive data from xml to list of dictionary
-    should be xml
-    should return a list of dictionary with data we need
-    data name : "eTag 配對路徑動態資訊(v2.0)" 的任意一份資料當作格式範例
-    reference data link : https://tisvcloud.freeway.gov.tw/history/motc20/ETag/20240302/ETagPairLive_2330.xml.gz
-    data source page link : https://tisvcloud.freeway.gov.tw/history/motc20/ETag/
-    """
+    def parse_ETagPairID(input_str):
+        input_str = str(input_str)
+        match = re.match(r'(\d{2})([A-Z])(\d+)([A-Z])-(\d{2})([A-Z])(\d+)([A-Z])', input_str)
+        if match:
+            highway = match.group(1) 
+            start_mileage = match.group(3) 
+            end_mileage = match.group(7) 
+            direction = match.group(4) 
+            return int(highway), int(start_mileage)/10, int(end_mileage)/10, direction
+        else:
+            return None, None, None, None
+        # end of parse_ETagPairID function 
+
+    # set up 
     print("\tProcess ETagPairLive data from file: ", store_path)
-    ###開始###
     print("store_path: ", store_path)
-    xml_data = open(store_path, 'rb').read()
+    xml_data = open(store_path, 'rb').read() # read xml file as web data
     root = ET.fromstring(xml_data)
     c = conn.db.cursor()
+    car_code_needed = [31, 32, 41, 42, 5] # 31小客車 32小貨車 41大客車 42大貨車 5聯結車
 
+    # create table
     c.execute('''
         CREATE TABLE IF NOT EXISTS ETagPairLive (
             ID              INTEGER PRIMARY KEY AUTOINCREMENT,
             ETagPairID      TEXT,
+            Highway         INTEGER,
+            StartMileage    REAL,
+            EndMileage      REAL,
+            Direction       CHAR(1),
             Year            INTEGER,
             Month           INTEGER,
             Day             INTEGER,
@@ -37,29 +74,29 @@ def convert_and_store_ETagPairLive(store_path, conn):
         )
     ''')
 
-    car_code_needed = [31, 32, 41, 42, 5] # 31小客車 32小貨車 41大客車 42大貨車 5聯結車
-
-    counter = 0
     for etag_pair_live in root.findall('.//{http://traffic.transportdata.tw/standard/traffic/schema/}ETagPairLive'):
-        """ xml EtagPair structure
+        """ 
+        xml EtagPair structure :
             ETagPairID TEXT,            store
-            StartETagStatus INTEGER,    logic 
-            EndETagStatus INTEGER,      logic
+            StartETagStatus INTEGER,    logic, and discard later
+            EndETagStatus INTEGER,      logic, and discard later
             StartTime TEXT,             store
-            EndTime TEXT,               don't need
-            DataCollectTime TEXT        don't need
+            EndTime TEXT,               don't need, always StartTime + 5 min
+            DataCollectTime TEXT        don't need, the same as EndTime
         """
+        # extract first level data 
         etag_pair_id = etag_pair_live.find('{http://traffic.transportdata.tw/standard/traffic/schema/}ETagPairID').text
         start_etag_status = int(etag_pair_live.find('{http://traffic.transportdata.tw/standard/traffic/schema/}StartETagStatus').text)
         end_etag_status = int(etag_pair_live.find('{http://traffic.transportdata.tw/standard/traffic/schema/}EndETagStatus').text)
         start_time = etag_pair_live.find('{http://traffic.transportdata.tw/standard/traffic/schema/}StartTime').text
 
-        # drop uneeded row_data
+        # drop uneeded invalid data
         if ((start_etag_status != 0) or (end_etag_status != 0)) :
             continue # two status should be normal 
 
         for flow in etag_pair_live.findall('.//{http://traffic.transportdata.tw/standard/traffic/schema/}Flow'):
-            """ xml Flow structure
+            """ 
+            xml Flow structure :
                 ETagPairID TEXT,            store
                 VehicleType INTEGER,        logic and store
                 TravelTime INTEGER,         don't need 
@@ -67,14 +104,17 @@ def convert_and_store_ETagPairLive(store_path, conn):
                 SpaceMeanSpeed INTEGER,     store
                 VehicleCount INTEGER,       store 
             """
+            # extract second level data
             vehicle_type = int(flow.find('{http://traffic.transportdata.tw/standard/traffic/schema/}VehicleType').text)
             space_mean_speed = int(flow.find('{http://traffic.transportdata.tw/standard/traffic/schema/}SpaceMeanSpeed').text)
             vehicle_count = int(flow.find('{http://traffic.transportdata.tw/standard/traffic/schema/}VehicleCount').text)
 
-            # drop uneeded row_data
+            # drop uneeded vehicle type data 
             if (vehicle_type not in car_code_needed) :
                 continue
-            # process time 
+            # process ETagPairID to highway, start_mileage, end_mileage, direction
+            highway, start_mileage, end_mileage, direction = parse_ETagPairID(etag_pair_id)
+            # process time to year, month, day, five_minute
             dt = datetime.fromisoformat(start_time)
             year = dt.year
             month = dt.month
@@ -82,28 +122,39 @@ def convert_and_store_ETagPairLive(store_path, conn):
             start_dt_of_day = dt
             start_dt_of_day = start_dt_of_day.replace(hour=0, minute=0, second=0, microsecond=0)
             five_minute = int((dt - start_dt_of_day).seconds / 300) # 5 min = 300 sec
+            # store data to database as ETagPairLive schema
             c.execute('''
-                INSERT INTO ETagPairLive (ETagPairID, Year, Month, Day, FiveMinute, VehicleType, SpaceMeanSpeed, VehicleCount)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            ''', (etag_pair_id, year, month, day, five_minute, vehicle_type, space_mean_speed, vehicle_count)
+                INSERT INTO ETagPairLive (ETagPairID, Highway, StartMileage, EndMileage, Direction, Year, Month, Day, FiveMinute, VehicleType, SpaceMeanSpeed, VehicleCount)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (etag_pair_id, highway, start_mileage, end_mileage, direction, year, month, day, five_minute, vehicle_type, space_mean_speed, vehicle_count)
             )
-            counter += 1
     conn.db.commit()
     print("Data successfully inserted into the SQLite3 database.")
-    print("Total rows inserted: ", counter)
-    ###結束###
+    # end of convert_and_store_ETagPairLive function
 
+
+"""
+    "convert_and_store_traffic_accident" : 
+        Process 交通事故簡訊通報狀況資料 data from xlsx to pandas dataframe and store in database
+    data file : xlsx
+    processed type(not return) : store in database (default name : row.db)
+    data information:
+        data name : "112年1-10月及113年1-2月交通事故簡訊通報狀況資料" 的分析資料或驗證資料的任意一份資料當作格式範例
+        reference data link : https://freeway2024.tw/112%E5%B9%B41-10%E6%9C%88%E4%BA%A4%E9%80%9A%E4%BA%8B%E6%95%85%E7%B0%A1%E8%A8%8A%E9%80%9A%E5%A0%B1%E8%B3%87%E6%96%99.xlsx
+        data source page link : https://freeway2024.tw/links#links
+    procedure : 
+        1. create traffic_accident table
+        2. parse xlsx to pandas dataframe
+        3. process data
+            * extract highway number
+            * convert direction to char
+            * one hot encoding for lane data
+            * drop those data with missing value (NaN or None)
+            * no need to precess time, already in correct format
+        4. change data type
+        5. pick up certain columns to database
+"""
 def convert_and_store_traffic_accident(store_path, conn):
-    """
-    Process traffic accident data from xlsx to list of dictionary
-    should be xlsx file
-    should return a list of dictionary with data we need
-    data name : "112年1-10月及113年1-2月交通事故簡訊通報狀況資料" 的分析資料或驗證資料的任意一份資料當作格式範例
-    reference data link : https://freeway2024.tw/112%E5%B9%B41-10%E6%9C%88%E4%BA%A4%E9%80%9A%E4%BA%8B%E6%95%85%E7%B0%A1%E8%A8%8A%E9%80%9A%E5%A0%B1%E8%B3%87%E6%96%99.xlsx
-    data source page link : https://freeway2024.tw/links#links
-    """
-    print("\tProcess traffic accident data from file: ", store_path)
-    ###開始###
     """ xlsx traffic accident structure
         年 INTEGER,             store
         月 INTEGER,             store
@@ -163,6 +214,7 @@ def convert_and_store_traffic_accident(store_path, conn):
             return match.group(1)
         else : 
             return None
+        # end of extract_highway_number function
     def convert_direction (direction) :
         direction = str(direction)
         if pd.isnull(direction):
@@ -175,24 +227,26 @@ def convert_and_store_traffic_accident(store_path, conn):
             return 'E'
         elif "西" in direction:
             return 'W'
+        else :
+            return None
+        # end of convert_direction function
     def one_hot (x) :
         if x == 1:
             return True
         else:
             return False
+        # end of one_hot function
     
-    # create table
+    print("\tProcess traffic accident data from file: ", store_path)
     create_table_query = '''
     CREATE TABLE IF NOT EXISTS traffic_accident (
         ID              INTEGER PRIMARY KEY AUTOINCREMENT,
         Highway         INTEGER,
         Direction       CHAR(1),
-        Mileage         INTEGER,
+        Mileage         REAL,
         Year            INTEGER,
         Month           INTEGER,
         Day             INTEGER,
-        Hour            INTEGER,
-        Minute          INTEGER,
         RecoveryMinute  INTEGER,
         FiveMinuteStart INTEGER,
         FiveMinuteEnd   INTEGER,
@@ -206,6 +260,7 @@ def convert_and_store_traffic_accident(store_path, conn):
         匝道            BOOLEAN
     )
     '''
+    # create table
     conn.db.cursor().execute(create_table_query)
     conn.db.commit()
     # pick up data from xlsx
@@ -239,7 +294,7 @@ def convert_and_store_traffic_accident(store_path, conn):
     data_subset['Month'] = data_subset['月'].astype(int)
     data_subset['Day'] = data_subset['日'].astype(int)
     data_subset['RecoveryMinute'] = data_subset['處理分鐘'].astype(int)
-    data_subset['Mileage'] = data_subset['里程'].astype(int)
+    data_subset['Mileage'] = data_subset['里程']
     for data in data_need_to_be_int:
         data_subset[data] = data_subset[data].astype(int)
     data_subset['Direction'] = data_subset['Direction'].astype(str).str[0]
@@ -253,19 +308,32 @@ def convert_and_store_traffic_accident(store_path, conn):
     data_subset[final_columns].to_sql('traffic_accident', conn.db, if_exists='replace', index=True)
 
     conn.db.commit()
-    ###結束###
+    # end of convert_and_store_traffic_accident function
 
+
+"""
+    "convert_and_store_construction_zone" : 
+        Process 施工路段資料 data from xlsx to pandas dataframe and store in database
+    data file : xlsx
+    processed type(not return) : store in database (default name : row.db)
+    data information:
+        data name : "112 年 1-10 月及 113 年 1-2 月施工路段資料"的分析資料或驗證資料的任意一份資料當作格式範例
+        reference data link : https://freeway2024.tw/112%E5%B9%B41-10%E6%9C%88%E9%81%93%E8%B7%AF%E6%96%BD%E5%B7%A5%E8%B7%AF%E6%AE%B5%E8%B3%87%E6%96%99.xlsx
+        data source page link : https://freeway2024.tw/links#links
+    procedure : 
+        1. create construction_zone table
+        2. parse xlsx to pandas dataframe
+        3. process data
+            * extract highway number
+            * convert direction to char
+            * process time to "start and end" of "year, month, day, five_minute"
+            * one hot encoding for lane data
+            * handle mileage data (set unit as km)
+            * drop those data with missing value (NaN or None)
+        4. change data type
+        5. pick up certain columns to database
+"""
 def convert_and_store_construction_zone(store_path, conn):
-    """
-    Process construction zone data from xlsx to list of dictionary
-    should be xlsx file
-    should return a list of dictionary with data we need
-    data name : "112 年 1-10 月及 113 年 1-2 月施工路段資料"的分析資料或驗證資料的任意一份資料當作格式範例
-    reference data link : https://freeway2024.tw/112%E5%B9%B41-10%E6%9C%88%E9%81%93%E8%B7%AF%E6%96%BD%E5%B7%A5%E8%B7%AF%E6%AE%B5%E8%B3%87%E6%96%99.xlsx
-    data source page link : https://freeway2024.tw/links#links
-    """
-    print("\tProcess construction zone data from file: ", store_path)
-    ###開始###
     """ xlsx construction zone structure
         incStepIncidentId INTEGER,      not need
         incStepNum INTEGER,             not need
@@ -296,6 +364,7 @@ def convert_and_store_construction_zone(store_path, conn):
             return int(10)
         else :
             return None
+        # end of extract_highway_number function
     def convert_direction (direction_number) :
         if pd.isnull(direction_number):
             return None
@@ -309,6 +378,7 @@ def convert_and_store_construction_zone(store_path, conn):
             return 'W'
         else :
             return None
+        # end of convert_direction function
     def one_hot (target, control) :
         control = str(control)
         dic = {"內側路肩": 1, "第1車道": 2, "第2車道": 3, "第3車道": 4, "第4車道": 5, "第5車道": 6, "第6車道": 7, "第7車道": 8, "第8車道": 9, "外側路肩": 10, "內邊坡":11, "外邊坡":12}
@@ -321,6 +391,9 @@ def convert_and_store_construction_zone(store_path, conn):
         elif target in dic:
             x = lambda control, target : True if control[dic[target]-1] == "1" else False
             return x(control, target)
+        else:
+            return None
+        # end of one_hot function
     def separate_time (target, control) :
         if len(str(control)) < 10 :
             return None
@@ -341,17 +414,20 @@ def convert_and_store_construction_zone(store_path, conn):
             return int(five_minute_of_day)
         else:
             return None
+        # end of separate_time function
     def handle_mileage (start_mileage, end_mileage) :
         if pd.isnull(start_mileage) or pd.isnull(end_mileage):
             return None, None
         if start_mileage > end_mileage:
             return None, None
         if start_mileage == 0:
-            return end_mileage, end_mileage
+            return end_mileage / 1000, end_mileage / 1000
         if end_mileage == 0:
-            return start_mileage, start_mileage
-        return start_mileage, end_mileage
+            return start_mileage / 1000, start_mileage / 1000
+        return start_mileage / 1000, end_mileage / 1000
+        # end of handle_mileage function
     
+    print("\tProcess construction zone data from file: ", store_path)
     # create table
     create_table_query = '''
     CREATE TABLE IF NOT EXISTS construction_zone (
@@ -366,8 +442,8 @@ def convert_and_store_construction_zone(store_path, conn):
         EndFiveMinute     INTEGER,
         Highway             INTEGER,
         Direction           CHAR(1),
-        StartMileage       INTEGER,
-        EndMileage         INTEGER,
+        StartMileage       REAL,
+        EndMileage         REAL,
         內側路肩             BOOLEAN,
         第1車道             BOOLEAN,
         第2車道              BOOLEAN,
@@ -403,10 +479,11 @@ def convert_and_store_construction_zone(store_path, conn):
     for route in route_list :
         data_subset[route] = data_subset.apply(lambda x : one_hot(route, x['incStepBlockagePattern']), axis=1)
     data_subset['StartMileage'], data_subset['EndMileage'] = zip(*data_subset.apply(lambda x : handle_mileage(x['incStepStartMileage'], x['incStepEndMileage']), axis=1))
-    
+    # 註 : pandas dataframe 這邊用到"多參數用法"與"多回傳值用法"，所以長得特別奇怪
+
     # change data type
     data_subset = data_subset.dropna()
-    data_need_to_be_int = ['StartYear', 'StartMonth', 'StartDay', 'StartFiveMinute', 'EndYear', 'EndMonth', 'EndDay', 'EndFiveMinute', 'Highway', 'StartMileage', 'EndMileage']
+    data_need_to_be_int = ['StartYear', 'StartMonth', 'StartDay', 'StartFiveMinute', 'EndYear', 'EndMonth', 'EndDay', 'EndFiveMinute', 'Highway']
     data_subset[data_need_to_be_int] = data_subset[data_need_to_be_int].astype(int)
     data_subset['Direction'] = data_subset['Direction'].astype(str).str[0]
     data_need_to_be_bool = route_list
@@ -419,4 +496,4 @@ def convert_and_store_construction_zone(store_path, conn):
     data_subset[final_columns].to_sql('construction_zone', conn.db, if_exists='replace', index=True)
     
     conn.db.commit()
-    ###結束###
+    # end of convert_and_store_construction_zone function
