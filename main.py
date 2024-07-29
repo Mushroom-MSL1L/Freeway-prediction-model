03b9def13d587e9647da60a0f717ee0b9f0c5b05
Lu Junyou <mushroom.ac.0520@gmail.com>
2024-07-29 08:24:45 +0800

    chore : add python init path

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from Database import Preprocess
-from Model import test_model_with_diabetes
+from Model import Model
 
 if __name__ == "__main__":
     # test_model_with_diabetes()
-    
+    m = Model()
     preprocess_var = Preprocess()
     preprocessed_data = preprocess_var.get_preprocessed_data()
     print(preprocessed_data.head())
+    
